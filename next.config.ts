a8ede3051a561910bc3766370354270474d81e1a
Icyubahiro-Jay-P <icyubahiro1980@gmail.com>
2026-09-20 18:33:10 +0200

    feat: add additional remote pattern for images from Cloudinary

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**.public.blob.vercel-storage.com" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
     ],
   },
 }
