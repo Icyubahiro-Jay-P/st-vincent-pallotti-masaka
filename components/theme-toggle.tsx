5c95fc08d31dfb4f360e743dd39bb534fe7f0485
Icyubahiro-Jay-P <icyubahiro1980@gmail.com>
2026-09-23 18:56:31 +0200

    refactor: use useMounted in theme toggle

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import * as React from "react"
 import { useTheme } from "next-themes"
 import { Moon, Sun } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 import type { Dictionary } from "@/lib/i18n/get-dictionary"
+import { useMounted } from "@/hooks/use-mounted"
 
 export function ThemeToggle({
   dict,
@@ -16,11 +16,7 @@ export function ThemeToggle({
   className?: string
 }) {
   const { resolvedTheme, setTheme } = useTheme()
-  const [mounted, setMounted] = React.useState(false)
-
-  React.useEffect(() => {
-    setMounted(true)
-  }, [])
+  const mounted = useMounted()
 
   const isDark = mounted && resolvedTheme === "dark"
 
